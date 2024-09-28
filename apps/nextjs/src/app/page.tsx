133bfc3805d87796154b84faa59d7f5777ee655b
wardxela <wardxela@gmail.com>
2024-09-28 23:04:41 +0300

    feat: openapi client generation

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,4 +1,10 @@
-export default function HomePage() {
+import api from "@rwx/api";
+
+export default async function HomePage() {
+  const result = await api
+    .GET("/", { parseAs: "text" })
+    .then((res) => res.data);
+
   return (
     <main className="container h-screen py-16">
       <div className="flex flex-col items-center justify-center gap-4">
@@ -7,6 +13,7 @@ export default function HomePage() {
           <span className="text-blue-400">Write</span>{" "}
           <span className="text-blue-600">Execute</span>
         </h1>
+        {result}
         <div className="w-full max-w-2xl overflow-y-scroll"></div>
       </div>
     </main>
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,3 +1,7 @@
-export const api = `
-  Here will be API SDK generating code
-`;
+import createClient from "openapi-fetch";
+
+import type { paths } from "./schema.d.ts";
+
+const api = createClient<paths>({ baseUrl: "http://localhost:3001" });
+
+export default api;
