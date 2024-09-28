f408b7f8a11420f40421af01d9b650a15b80cbac
wardxela <wardxela@gmail.com>
2024-09-28 23:17:16 +0300

    feat: husky pre-commit hook

--- packages/api/eslint.config.js
+++ packages/api/eslint.config.js
@@ -3,7 +3,7 @@ import baseConfig from "@rwx/eslint-config/base";
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: ["dist/**"],
+    ignores: ["src/schema.d.ts"],
   },
   ...baseConfig,
 ];
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -2,10 +2,4 @@ import baseConfig from "@rwx/eslint-config/base";
 import reactConfig from "@rwx/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: ["dist/**"],
-  },
-  ...baseConfig,
-  ...reactConfig,
-];
+export default [...baseConfig, ...reactConfig];
