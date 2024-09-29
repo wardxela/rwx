9d734ce7ecd154c84703fe752cd007ad47e9701d
wardxela <wardxela@gmail.com>
2024-09-29 12:48:55 +0300

    fix(nestjs): no reason to create a swagger api endpoint - remove it

--- apps/nestjs/src/main.ts
+++ apps/nestjs/src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle("RWX API")
-    .setDescription("RWX API description")
-    .setVersion("1.0")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("swagger", app, document);
 
   await app.listen(3001);
 }
