3ce52d7b54225118066d5c1affb317967284c11c
wardxela <wardxela@gmail.com>
2024-09-29 12:57:09 +0300

    fix(dev): run format in pre-commit hook and include schema.d.ts in eslint config

--- packages/api/eslint.config.js
+++ packages/api/eslint.config.js
@@ -1,9 +1,4 @@
 import baseConfig from "@rwx/eslint-config/base";
 
 /** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: ["src/schema.d.ts"],
-  },
-  ...baseConfig,
-];
+export default [...baseConfig];
