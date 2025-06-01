4b1fbe16eddcf9ed793f7aa001d434afddd5f337
wardxela <wardxela@gmail.com>
2025-06-01 14:55:32 +0300

    chore: update dependencies

--- apps/solidjs/app.config.ts
+++ apps/solidjs/app.config.ts
@@ -2,7 +2,9 @@ import path from "node:path";
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-  server: {},
+  server: {
+    preset: "bun",
+  },
   vite: {
     envDir: path.join("..", ".."),
   },
