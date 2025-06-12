90f7e03b2a1f9e94e069ab248c4c31bf84328495
wardxela <wardxela@gmail.com>
2025-06-12 17:01:57 +0300

    feat(dev): add solid-devtools to SolidJS app

--- apps/solidjs/app.config.ts
+++ apps/solidjs/app.config.ts
@@ -1,9 +1,16 @@
 import path from "node:path";
 import { defineConfig } from "@solidjs/start/config";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
   middleware: "src/middleware.ts",
   vite: {
     envDir: path.join("..", ".."),
+    plugins: [
+      devtools({
+        /* features options - all disabled by default */
+        autoname: true, // e.g. enable autoname
+      }),
+    ],
   },
 });
--- apps/solidjs/src/entry-client.tsx
+++ apps/solidjs/src/entry-client.tsx
@@ -1,4 +1,5 @@
 // @refresh reload
 import { StartClient, mount } from "@solidjs/start/client";
+import "solid-devtools";
 
 export default mount(() => <StartClient />, document.getElementById("app")!);
